b715fedd13c83c572c6fed5b8c9ed21aafe13cb3
Eldridge0831 <85918493+Eldridge0831@users.noreply.github.com>
2021-12-19 12:44:34 -0500

    troubleshooting category fetch

---
+++ src/components/CategorySearch.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+
+// const CategorySearch = () => {
+
+//     const [categoryOptions, setCategoryOptions] = useState("")
+
+
+//     Object.keys(cookbookItem).forEach(function (recipe) {
+//         item = cookbookItem[category]
+//     }
+//     );
+
+
+//     return (
+//         <div>
+//             <select onChange={handleCategorySelection}>
+//                 <option value={categoryOptions}>Sort by Category</option>
+//                 {/* Mapping through each fruit object in our fruits array
+//                 and returning an option element with the appropriate attributes / values.
+//                  */}
+//                 {fruits.map((fruit) => <option value={fruit.value}>{fruit.label}</option>)}
+//             </select>
+//         </div>
+//     )
+// }
+
+// export default CategorySearch
