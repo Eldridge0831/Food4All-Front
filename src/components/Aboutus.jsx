15146b9e73da2232f7ddf7d2590a9947ee85136b
Vivek Thakore <vivek.thakore08@gmail.com>
2021-12-19 20:19:00 -0600

    updating about us

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { Github, Linkedin, EnvelopeFill } from "react-bootstrap-icons";
+import Vivek from '../assets/vivek.jpg';
 
 function Aboutus () {
     return(
@@ -13,13 +15,12 @@ function Aboutus () {
         <div class="column">
           <div class="card">
             <div class="container" styles="text-align:center">
+                <img class="card-img-top w-100 d-block" alt = "Profile" src = {Vivek}></img>
               <h2>Vivek Thakore</h2>
               <p class="title">Web Developer</p>
-              {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
-              <p>vivek.thakore08@gmail.com</p>
-              <a href = "https://github.com/vivekthak88">https://github.com/vivekthak88</a>
-              <a href = "https://www.linkedin.com/in/vivekthakore">https://linkedIn.com/</a>
-              {/* <!-- <p><button class="button">Contact</button></p> --> */}
+              <a href = "mailto: vivek.thakore08@gmail.com" target="_blank" rel="noreferrer"> <EnvelopeFill size={40} className="p-1"/> </a>
+              <a href = "https://github.com/vivekthak88"><Github size={40} className="p-1"/></a>
+              <a href = "https://www.linkedin.com/in/vivekthakore"><Linkedin size={36} className="p-1" /></a>
             </div>
           </div>
         </div>
@@ -27,13 +28,15 @@ function Aboutus () {
         <div class="column">
           <div class="card">
             <div class="container" styles="text-align:center">
+                <img src = ""></img>
               <h2>Charles Eldrigde</h2>
               <p class="title">Web Developer</p>
-              {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
-              <p>Charles Email</p>
-              <a href = "https://github.com/Eldridge0831">https://github.com/</a>
-              <a href = "">https://linkedIn.com/</a>
-              {/* <!-- <p><button class="button">Contact</button></p> --> */}
+              <a href="mailto: " target="_blank" rel="noreferrer">
+                  <EnvelopeFill size={40} class="p-1" /></a>
+              <a href = "https://github.com/Eldridge0831" target="_blank" rel="noreferrer">
+                  <Github size={40} class="p-1" /></a>
+              <a href = "" target="_blank" rel="noreferrer">
+                  <Linkedin size={36} class="p-1" /></a>
             </div>
           </div>
         </div>
