f832ef64368bda83f0b9bfc931a639899f5f9e34
Vivek Thakore <vivek.thakore08@gmail.com>
2021-12-21 11:00:28 -0600

    added about us

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,59 +1,163 @@
-import React from 'react'
+import React from "react";
+import { Card, ListGroupItem,} from "react-bootstrap";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Github, Linkedin, EnvelopeFill } from "react-bootstrap-icons";
+import Vivek from "../assets/vivek.jpg";
+import Charles from "../assets/charles.jpg";
+import Tim from "../assets/tim.jpg";
 
-function Aboutus () {
-    return(
-        <>
-    <div class="about-section">
-        <h1>Contact Us</h1>
-        <p>Food 4 All</p>
-      </div>
-      
-      <h2 styles="text-align:center">Our Team</h2>
-      <div class="row">
-        <div class="column">
-          <div class="card">
-            <div class="container" styles="text-align:center">
-              <h2>Vivek Thakore</h2>
-              <p class="title">Web Developer</p>
-              {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
-              <p>vivek.thakore08@gmail.com</p>
-              <a href = "https://github.com/vivekthak88">https://github.com/vivekthak88</a>
-              <a href = "https://www.linkedin.com/in/vivekthakore">https://linkedIn.com/</a>
-              {/* <!-- <p><button class="button">Contact</button></p> --> */}
-            </div>
-          </div>
-        </div>
-      
-        <div class="column">
-          <div class="card">
-            <div class="container" styles="text-align:center">
-              <h2>Charles Eldrigde</h2>
-              <p class="title">Web Developer</p>
-              {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
-              <p>Charles Email</p>
-              <a href = "https://github.com/Eldridge0831">https://github.com/</a>
-              <a href = "">https://linkedIn.com/</a>
-              {/* <!-- <p><button class="button">Contact</button></p> --> */}
-            </div>
-          </div>
-        </div>
-      
-        <div class="column">
-          <div class="card">
-            <div class="container" styles="text-align:center">
-              <h2>Tim Johns</h2>
-              <p class="title">Web Developer</p>
-              {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
-              <p>Tim's email</p>
-              <a href = "https://github.com/tjohns823">https://github.com/</a>
-              <a href = "">https://linkedIn.com/</a>
-              {/* <!-- <p><button class="button">Contact</button></p> --> */}
-            </div>
-          </div>
-        </div>
-      </div>
-      </>
-    )
+function Aboutus() {
+  return (
+    <div className= "about-us-container d-flex align-items-center justify-content-center mt-5">
+      <Card  border="primary" style={{ maxWidth: '20rem' }}>
+        <Card.Img variant="top" src={Vivek} />
+        <Card.Body>
+          <Card.Title>Vivek Thakore</Card.Title>
+          <Card.Text>Web Developer</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>
+            <a
+              href="mailto: vivek.thakore08@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <EnvelopeFill size={40} className="p-1" />
+            </a>
+          </ListGroupItem>
+          <ListGroupItem>
+            <a href="https://github.com/vivekthak88">
+              <Github size={40} className="p-1" />
+            </a>
+          </ListGroupItem>
+          <ListGroupItem>
+            <a href="https://www.linkedin.com/in/vivekthakore">
+              <Linkedin size={36} className="p-1" />
+            </a>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+
+      <Card border="primary" style={{ maxWidth: '20rem' }}>
+        <Card.Img variant="top" src={Charles} />
+        <Card.Body>
+          <Card.Title>Charles Eldrigde</Card.Title>
+          <Card.Text>Web Developer</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>
+            <a
+              href="mailto: eldridge.crow@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <EnvelopeFill size={40} className="p-1" />
+            </a>
+          </ListGroupItem>
+          <ListGroupItem>
+            <a href="https://github.com/Eldridge0831">
+              <Github size={40} className="p-1" />
+            </a>
+          </ListGroupItem>
+          <ListGroupItem>
+            <a href="https://www.linkedin.com/in/vivekthakore">
+              <Linkedin size={36} className="p-1" />
+            </a>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+
+      <Card border="primary" style={{ maxWidth: '20rem' }}>
+        <Card.Img variant="top" src={Tim} />
+        <Card.Body>
+          <Card.Title>Tim Johns</Card.Title>
+          <Card.Text>Web Developer</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>
+            <a
+              href="mailto: tjohns823@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <EnvelopeFill size={40} className="p-1" />
+            </a>
+          </ListGroupItem>
+          <ListGroupItem>
+            <a href="https://github.com/tjohns823">
+              <Github size={40} className="p-1" />
+            </a>
+          </ListGroupItem>
+          <ListGroupItem>
+            <a href=" https://www.linkedin.com/in/timothy-johns-jr/">
+              <Linkedin size={36} className="p-1" />
+            </a>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+    </div>
+  );
 }
 
 export default Aboutus;
+
+
+// import React from 'react'
+
+// function Aboutus () {
+//     return(
+//         <>
+//     <div class="about-section">
+//         <h1>Contact Us</h1>
+//         <p>Food 4 All</p>
+//       </div>
+      
+//       <h2 styles="text-align:center">Our Team</h2>
+//       <div class="row">
+//         <div class="column">
+//           <div class="card">
+//             <div class="container" styles="text-align:center">
+//               <h2>Vivek Thakore</h2>
+//               <p class="title">Web Developer</p>
+//               {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
+//               <p>vivek.thakore08@gmail.com</p>
+//               <a href = "https://github.com/vivekthak88">https://github.com/vivekthak88</a>
+//               <a href = "https://www.linkedin.com/in/vivekthakore">https://linkedIn.com/</a>
+//               {/* <!-- <p><button class="button">Contact</button></p> --> */}
+//             </div>
+//           </div>
+//         </div>
+      
+//         <div class="column">
+//           <div class="card">
+//             <div class="container" styles="text-align:center">
+//               <h2>Charles Eldrigde</h2>
+//               <p class="title">Web Developer</p>
+//               {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
+//               <p>Charles Email</p>
+//               <a href = "https://github.com/Eldridge0831">https://github.com/</a>
+//               <a href = "">https://linkedIn.com/</a>
+//               {/* <!-- <p><button class="button">Contact</button></p> --> */}
+//             </div>
+//           </div>
+//         </div>
+      
+//         <div class="column">
+//           <div class="card">
+//             <div class="container" styles="text-align:center">
+//               <h2>Tim Johns</h2>
+//               <p class="title">Web Developer</p>
+//               {/* <!-- <p>Some text that describes me lorem ipsum ipsum lorem.</p> --> */}
+//               <p>Tim's email</p>
+//               <a href = "https://github.com/tjohns823">https://github.com/</a>
+//               <a href = "">https://linkedIn.com/</a>
+//               {/* <!-- <p><button class="button">Contact</button></p> --> */}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       </>
+//     )
+// }
+
+// export default Aboutus;
